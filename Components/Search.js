f54712380cb33f6e86c88c6f63c6d35232e66e8b
Rambuteau <nanar445@hotmail.fr>
2020-02-22 09:46:52 +0100

    Modification Component Search Scroll view

--- Components/Search.js
+++ Components/Search.js
@@ -1,41 +1,81 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
-import films from '../Helpers/filmsData'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
-
 
 class Search extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state= {
-      film:[]
+    this.searchedText = ""
+    this.page = 0
+    this.totalPages = 0
+    this.state = {
+      films: [],
+      isLoading: false
     }
-    this.searchText=""
   }
 
   _loadFilms() {
-    if (this.searchText.length > 0){
-      getFilmsFromApiWithSearchedText(this.searchText).then(data => this.setState({ films: data.results}))
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+          this.page = data.page
+          this.totalPages = data.total_pages
+          this.setState({
+            films: [ ...this.state.films, ...data.results ],
+            isLoading: false
+          })
+      })
     }
   }
- 
-  _searchTextInputChanged(text){
-    this.searchText = text
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text 
   }
- //permet de récuperer le text écrit dans la bar de recherche et que le stat serachText change 
+
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      films: [],
+    }, () => {
+        this._loadFilms()
+    })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
 
   render() {
     return (
       <View style={styles.main_container}>
-        <TextInput onChangeText={(text)=>this._searchTextInputChanged(text)} style={styles.textinput} placeholder='Titre du film'/>
-        <Button style={{height:50 }}title='Rechercher' onPress={() => this._loadFilms()}/>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}
+        />
+        <Button title='Rechercher' onPress={() => this._searchFilms()}/>
         <FlatList
-        data={this.state.films}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({item}) => <FilmItem film= {item}/>}
-      />
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.page < this.totalPages) {
+                 this._loadFilms()
+              }
+          }}
+        />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -53,6 +93,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
