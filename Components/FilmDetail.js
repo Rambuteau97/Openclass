50237f7881c17b4b8bd5d555cddd79e8f06a7b57
Rambuteau <nanar445@hotmail.fr>
2020-02-23 10:27:18 +0100

    Afficher titre du film en cliquant dessus

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,11 +1,50 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native'
+import { getFilmDetailFromApi } from '../API/TMDBApi'
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      film: undefined,
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  _displayFilm() {
+    if (this.state.film != undefined) {
+      return (
+        <ScrollView style={styles.scrollview_container}>
+          <Text>{this.state.film.title}</Text>
+        </ScrollView>
+      )
+    }
+  }
+
   render() {
     return (
       <View style={styles.main_container}>
-        <Text>Détail du film {this.props.navigation.state.params.idFilm}</Text>
+        {this._displayLoading()}
+        {this._displayFilm()}
       </View>
     )
   }
@@ -13,7 +52,19 @@ class FilmDetail extends React.Component {
 
 const styles = StyleSheet.create({
   main_container: {
-    flex: 1,
+    flex: 1
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  scrollview_container: {
+    flex: 1
   }
 })
 
